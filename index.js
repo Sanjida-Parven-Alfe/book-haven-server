572b6875c7cde2c41fe20f0da0177c6ad867d4cd
Sanjida Parven Alfe <sanjidaparvinalfe@gmail.com>
2025-11-13 13:11:22 +0600

    Get commit

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = 3000
 
 app.use(cors())
 app.use(express.json())
-
-
-
 
 const uri = "mongodb+srv://book_haven_db:UDsLM2ooefnyhv7O@cluster0.5scl4km.mongodb.net/?appName=Cluster0";
 
@@ -23,22 +20,48 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
 
     const db = client.db('book_haven_db')
-    const modelCollection = db.collection('Books')
+    const bookCollection = db.collection('Books')
 
+    // Get all books
     app.get('/Books', async (req, res) => {
-      
-      const result = await 
-      modelCollection.find().toArray()
-      res.send(result)
-
+      try {
+        const result = await bookCollection.find().toArray()
+        res.send(result)
+      } catch (error) {
+        res.status(500).send({ message: "Failed to fetch books" })
+      }
     })
 
+    // post method
+    //insertMany
+    //insertOne
 
+    app.post('/Books', (req, res) =>{
+      const data = req.body
+      console.log(data)
+      // const result = bookCollection.insertOne('') 
+      res.send({
+        success: true
+      })
+    } )
 
+    // Get book by ID
+    app.get('/Books/:id', async (req, res) => {
+      const { id } = req.params
+      try {
+        const book = await modelCollection.findOne({ _id: new ObjectId(id) })
+        if (!book) {
+          return res.status(404).send({ message: "Book not found" })
+        }
+        res.send(book)
+      } catch (error) {
+        res.status(500).send({ message: "Invalid book ID" })
+      }
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -50,7 +73,7 @@ async function run() {
 }
 run().catch(console.dir);
 
-
+// Test server
 app.get('/', (req, res) => {
   res.send('Server is running')
 })
